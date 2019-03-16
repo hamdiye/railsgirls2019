2be088e3c2261127984cfc74cebac164cd219b66
hamdie <hamdyekaya@gmail.com>
2019-03-16 16:52:17 +0300

    Add picture file input for idea model

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,3 @@
 class Idea < ApplicationRecord
+  mount_uploader :picture, PictureUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: redirect('/ideas')
   resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
